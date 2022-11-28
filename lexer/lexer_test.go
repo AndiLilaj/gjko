b1371b6932b6357298d5de2005af17eaaaf7159b
Andi <andililajal@gmail.com>
2022-11-28 19:29:50 +0100

    updated test to include new tokens

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -12,22 +12,19 @@ func TestNextToken(t *testing.T) {
 	let add = fn(x,y) {
 		x + y;
 	};
-	let rezult = add(five, ten);
+	let result = add(five, ten);
 		`
 
 	tests := []struct {
 		expectedType    token.TokenType
 		expectedLiteral string
 	}{
-		{token.LET, "let"}, {token.IDENT, "five"}, {token.ASSIGN, "="},
-		{token.INT, "5"}, {token.SEMICOLON, ";"}, {token.IDENT, "ten"},
-		{token.ASSIGN, "="}, {token.INT, "10"}, {token.SEMICOLON, ";"},
+		{token.LET, "let"}, {token.IDENT, "five"}, {token.ASSIGN, "="}, {token.INT, "5"}, {token.SEMICOLON, ";"},
+		{token.LET, "let"}, {token.IDENT, "ten"}, {token.ASSIGN, "="}, {token.INT, "10"}, {token.SEMICOLON, ";"},
 		{token.LET, "let"}, {token.IDENT, "add"}, {token.ASSIGN, "="},
-		{token.LET, "let"}, {token.FUNCTION, "fn"}, {token.LPAREN, "("},
-		{token.IDENT, "x"}, {token.COMMA, ","}, {token.IDENT, "y"},
-		{token.RPAREN, ")"}, {token.LBRACE, "{"}, {token.IDENT, "x"},
-		{token.PLUS, "+"}, {token.IDENT, "y"}, {token.SEMICOLON, ";"},
-		{token.RBRACE, "}"}, {token.SEMICOLON, ";"}, {token.LET, "let"},
+		{token.FUNCTION, "fn"}, {token.LPAREN, "("}, {token.IDENT, "x"}, {token.COMMA, ","}, {token.IDENT, "y"}, {token.RPAREN, ")"},
+		{token.LBRACE, "{"}, {token.IDENT, "x"}, {token.PLUS, "+"}, {token.IDENT, "y"}, {token.SEMICOLON, ";"}, {token.RBRACE, "}"}, {token.SEMICOLON, ";"},
+		{token.LET, "let"},
 		{token.IDENT, "result"}, {token.ASSIGN, "="}, {token.IDENT, "add"},
 		{token.LPAREN, "("}, {token.IDENT, "five"}, {token.COMMA, ","},
 		{token.IDENT, "ten"}, {token.RPAREN, ")"}, {token.SEMICOLON, ";"},
